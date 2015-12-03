7097d27d82486956e863d97f4b48e8d5622b442a
Michael Teichner <michael.r.teichner@gmail.com>
2015-12-03 16:24:38 +0200

    Pages CRUD and Navigation bar buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,51 @@
 class PagesController < ApplicationController
 
-	def index
+	before_action :find_page, only: [:show, :edit, :update, :destroy]
 
+	def index
+		@pages = Page.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@page = Page.new
+	end
+
+	def create
+		@page = Page.new(page_params)
+
+		if @page.save
+			redirect_to @page
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @page.update(page_params)
+			redirect_to @page
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@page.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def page_params
+		params.require(:page).permit(:name)
+	end
+
+	def find_page
+		@page = Page.find(params[:id])
 	end
 end
